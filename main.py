e236dc2da99bd39e9089fb3c9ca7c700e473e712
SrishtikaB <Bhatnagar.srish@gmail.com>
2026-02-15 18:55:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,100 @@
+import re
+from urllib.parse import urlparse
+
+KNOWN_BRANDS = [
+    "google", "facebook", "amazon", "paypal", "microsoft",
+    "apple", "netflix", "instagram", "linkedin", "twitter",
+    "spotify", "github", "adobe", "bank", "hdfc", "icici", "sbi"
+]
+
+SUSPICIOUS_WORDS = [
+    "login", "verify", "secure", "update", "account",
+    "bank", "confirm", "password", "signin", "alert", "reset"
+]
+
+SUSPICIOUS_TLDS = [
+    ".xyz", ".top", ".click", ".work", ".gq", ".tk", ".ml"
+]
+
+
+def phishing_score(url):
+    score = 0
+    reasons = []
+
+    url = url.lower()
+    parsed = urlparse(url)
+    domain = parsed.netloc if parsed.netloc else parsed.path
+
+   
+    if re.search(r"\d{1,3}(\.\d{1,3}){3}", url):
+        score += 25
+        reasons.append("IP address used")
+
+   
+    if "@" in url:
+        score += 25
+        reasons.append("@ symbol present")
+
+  
+    if "xn--" in domain:
+        score += 25
+        reasons.append("Punycode/homograph detected")
+
+    
+    if any(domain.endswith(tld) for tld in SUSPICIOUS_TLDS):
+        score += 15
+        reasons.append("Suspicious domain extension")
+
+   
+    for brand in KNOWN_BRANDS:
+        if brand in domain:
+            if any(word in url for word in SUSPICIOUS_WORDS):
+                score += 20
+                reasons.append("Brand + suspicious keyword")
+                break
+
+    
+    if any(word in url for word in SUSPICIOUS_WORDS):
+        score += 10
+        reasons.append("Suspicious keywords")
+
+   
+    if domain.count(".") > 3:
+        score += 10
+        reasons.append("Too many subdomains")
+
+   
+    if len(domain) > 25:
+        score += 8
+        reasons.append("Long domain name")
+
+    
+    if domain.count("-") >= 2:
+        score += 8
+        reasons.append("Multiple hyphens")
+
+   
+    if url.count(".") > 4:
+        score += 8
+        reasons.append("Too many dots in URL")
+
+    
+    if score >= 60:
+        verdict = "Phishing"
+    elif score >= 30:
+        verdict = "Suspicious"
+    else:
+        verdict = "Safe"
+
+    return score, verdict, reasons
+
+
+
+url = input("Enter URL: ")
+
+score, verdict, reasons = phishing_score(url)
+
+print("\nResult:")
+print("Score:", score)
+print("Verdict:", verdict)
+print("Reasons:", reasons)
